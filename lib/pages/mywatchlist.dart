a863fb77b2e562704009d1376b4b87540afcbc6a
gibranoderenz <gibranoderenz123@gmail.com>
2022-11-21 19:05:52 +0700

    Remove redundant imports

--- lib/pages/mywatchlist.dart
+++ lib/pages/mywatchlist.dart
@@ -1,6 +1,3 @@
-import 'dart:convert';
-
-import 'package:http/http.dart' as http;
 import 'package:counter_7/models/watchlist_item.dart';
 import 'package:flutter/material.dart';
 import 'package:counter_7/widgets/custom_drawer.dart';
